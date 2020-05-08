f53134463a33589f7f75b2b74d96adba54f814a8
Ivan <vanchas@protonmail.com>
2020-05-08 13:34:18 +0300

    domen changed

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,86 +1,88 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const domen = `http://avtotest.org/public`;
+
 module.exports = function (app) {
   app.use("/api",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/blog/posts",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/blog/post/:id",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/blog/post/add",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/blog/post/update/:id",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/blog/post/:id",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/search",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/fb",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/fields/update/:key",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/register",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/logout",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
   );
   app.use("/api/login",
     createProxyMiddleware({
-      target: "https://strateg.link/public",
+      target: domen,
       secure: false,
       changeOrigin: true
     })
